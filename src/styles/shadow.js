bc5a578f98c24ba3b24aa9cc54097fbd92763fe0
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-04-18 18:40:21 +0200

    fix: fix elevation shadow on iOS

--- src/styles/shadow.js
+++ src/styles/shadow.js
@@ -3,27 +3,13 @@
 import * as Colors from './colors';
 
 export default function shadow(elevation: number) {
-  let height, radius;
-  switch (elevation) {
-    case 1:
-      height = 0.5;
-      radius = 0.75;
-      break;
-    case 2:
-      height = 0.75;
-      radius = 1.5;
-      break;
-    default:
-      height = elevation - 1;
-      radius = elevation;
-  }
   return {
     shadowColor: Colors.black,
+    shadowOpacity: 0.0015 * elevation + 0.18,
+    shadowRadius: 0.54 * elevation,
     shadowOffset: {
+      height: 0.6 * elevation,
       width: 0,
-      height,
     },
-    shadowOpacity: 0.24,
-    shadowRadius: radius,
   };
 }
